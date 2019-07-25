ca37a6f9cf22d88c236162b91b0d98725eaf390f
Matt Stauffer <matt@tighten.co>
2019-07-25 15:15:56 -0400

    Add opengraph logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <meta property="og:title" content="{{ $ogTitle ?? 'Onramp to Laravel' }}">
         <meta property="og:type" content="{{ $ogType ?? 'website' }}">
         <meta property="og:url" content="{{ $ogUrl ?? 'https://onramp.dev' }}">
-        {{-- <meta property="og:image" content="{{ $ogImage ?? 'change-me.jpg' }}"> --}}
+        <meta property="og:image" content="{{ $ogImage ?? '/images/opengraph_logo.png' }}">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>

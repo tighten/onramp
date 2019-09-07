209ee8559b1f5370ca7379beebb6f8f25fe007da
Matt Stauffer <matt@tighten.co>
2019-09-07 12:08:40 -0400

    Register Bugsnag Service Provider

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
 
         /*
          * Application Service Providers...

f8978c341fbb29031fe195469b003721783e66ab
Matt Stauffer <matt@tighten.co>
2019-09-07 12:39:13 -0400

    Don't report invalid locale exceptions

--- app/Exceptions/InvalidLocale.php
+++ app/Exceptions/InvalidLocale.php
@@ -6,6 +6,11 @@ use Exception;
 
 class InvalidLocale extends Exception
 {
+    public function report()
+    {
+        // Do nothing; we don't need to send this up to Bugsnag
+    }
+
     public function render($request)
     {
         return redirect('/');

13e3260edaece09cf4806ac3e4855d255d651307
Matt Stauffer <matt@tighten.co>
2019-10-15 11:53:41 -0400

    Add test for functioning glossary page, drop test covering package

--- tests/Feature/TermsTest.php
+++ tests/Feature/TermsTest.php
@@ -11,11 +11,20 @@ class TermsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function glossary_terms_can_be_multi_lingual()
+    function glossary_page_loads()
     {
         $term = factory(Term::class)->create();
 
-        $this->get('/en/glossary')
+        $response = $this->get('/en/glossary')
+            ->assertSuccessful();
+    }
+
+    /** @test */
+    function glossary_terms_can_be_multi_lingual()
+    {
+        $term = factory(Term::class)->create();
+
+        $response = $this->get('/en/glossary')
             ->assertSee($term->name);
 
         $this->get('/es/glossary')
@@ -24,28 +33,15 @@ class TermsTest extends TestCase
     }
 
     /** @test */
-    public function a_term_that_only_exists_in_english_shows_it_in_english_on_other_locales()
+    function a_term_that_only_exists_in_english_shows_it_in_english_on_other_locales()
     {
         $term = factory(Term::class)->create([
-            'name' => 'Routes',
-            'description' => 'This should be some information about the routes'
+            'name' => ['en' => 'Routes'],
+            'description' => ['en' => 'This should be some information about the routes'],
         ]);
 
         $this->get('/es/glossary')
             ->assertSee($term->name)
             ->assertSee($term->description);
-    }
-
-    /** @test */
-    public function a_term_without_specified_language_is_stored_with_the_default_locale()
-    {
-        $collections = [
-            'name' => 'Collections',
-            'description' => 'A collection is super powered arrays in an object oriented way'
-        ];
-
-        $term = Term::create($collections);
-        $this->assertEquals($collections['name'], $term->name);
-        $this->assertEquals($collections['name'], $term->getEnglishName());
     }
 }

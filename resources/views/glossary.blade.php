0e0dbf44b1268c9e3fc6bb75b2013a6b32177bcc
Jonas Erlandsson <jonas.erlandsson@sitedirect.se>
2019-10-16 13:52:34 +0200

    update indentation in glossary.blade.php

--- resources/views/glossary.blade.php
+++ resources/views/glossary.blade.php
@@ -23,7 +23,7 @@
                                 {{ $term->getTranslation('name', locale()) }}
                             </h3>
                             @if ($term->name !== $term->getEnglishName())
-                            <span class="pl-1 text-grey-800 capitalize text-sm">({{ $term->getEnglishName() }} )</span>
+                                <span class="pl-1 text-grey-800 capitalize text-sm">({{ $term->getEnglishName() }} )</span>
                             @endif
                         </div>
                         <p class="mt-2">{{ $term->getTranslation('description', locale()) }}</p>
@@ -38,11 +38,11 @@
                     <h3 class="text-xl">{{ __('Table of contents') }}</h3>
                     <ul class="mt-4">
                         @forelse ($terms as $term)
-                        <li class="leading-relaxed">
-                            <a class="capitalize" href="#{{ $term->getEnglishName() }}">{{ $term->name }}</a>
-                        </li>
+                            <li class="leading-relaxed">
+                                <a class="capitalize" href="#{{ $term->getEnglishName() }}">{{ $term->name }}</a>
+                            </li>
                         @empty
-                        {{ __('No terms') }}
+                            {{ __('No terms') }}
                         @endforelse
                     </ul>
                 </div>

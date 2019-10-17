4aff0a67d756ef616035d9907055b193837ca7ff
Jon Sugar <jonathansugar@mac.com>
2019-10-17 17:01:29 +0100

    Throw an exception when attempting to resolve a language for an invalid locale.

--- tests/Feature/LocaleTest.php
+++ tests/Feature/LocaleTest.php
@@ -24,13 +24,28 @@ class LocaleTest extends TestCase
     }
 
     /** @test */
+    public function if_throws_an_exception_when_resolving_a_language_for_an_invalid_locale()
+    {
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Cannot resolve language for locale: invalid_locale');
+
+        $locales = new Locale;
+        $locales->getLanguageForLocale('invalid_locale');
+
+    }
+
+    /** @test */
     public function it_retrieves_the_proper_language_for_a_given_locale()
     {
         $locales = new Locale;
 
+        // TODO: Replace language names with translated variants.
         $this->assertEquals('English', $locales->getLanguageForLocale('en'));
         $this->assertEquals('Spanish', $locales->getLanguageForLocale('es'));
         $this->assertEquals('Portuguese', $locales->getLanguageForLocale('pt'));
         $this->assertEquals('Swedish', $locales->getLanguageForLocale('sv'));
+        $this->assertEquals('German', $locales->getLanguageForLocale('de'));
+        $this->assertEquals('Danish', $locales->getLanguageForLocale('da'));
     }
 }

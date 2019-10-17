99a30aab3af0009df4d134b54c200828e0d8fcde
Jon Sugar <jonathansugar@mac.com>
2019-10-17 12:39:41 +0100

    Remove unused flags to prevent redundant files being added to Onramp if the pull request is merged.

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,8 +18,8 @@ mix.js('resources/js/app.js', 'public/js')
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.config.js')],
     })
-    .copy('resources/sass/vendor/flag-icon-css/flag-icon.css', 'public/css')
-    .copy('resources/sass/vendor/flag-icon-css/flags', 'public/flags')
+    .copy('resources/css/vendor/flag-icon-css/flag-icon.css', 'public/css')
+    .copy('resources/css/vendor/flag-icon-css/flags', 'public/flags')
     .browserSync('https://onramp.test');
 
 if (mix.inProduction()) {

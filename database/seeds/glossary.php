dd718963324f767495658dc05a8edb992efb9790
Jon Sugar <jonathansugar@mac.com>
2019-10-18 20:08:51 +0100

    Add back database seeding for glossary terms.

---
+++ database/seeds/glossary.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    [
+        'name' => [
+            'en' => 'collections',
+            'es' => 'colecciones',
+        ],
+        'description' => [
+            'en' => 'A Collection is a convenient wrapper for working with arrays of data.',
+            'es' => 'Collection description but in Spanish',
+        ],
+    ],
+    [
+        'name' => [
+            'es' => 'HTML',
+        ],
+        'description' => [
+            'en' => 'HTML description in english',
+            'es' => 'HTML description but in Spanish',
+            'de' => 'HTML description but in German',
+        ],
+    ],
+    [
+        'name' => [
+            'en' => 'CSS',
+        ],
+        'description' => [
+            'en' => 'CSS description in english',
+        ],
+    ],
+];

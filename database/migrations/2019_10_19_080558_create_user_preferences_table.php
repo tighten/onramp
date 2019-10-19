af27557adfbe15f690ad23a9dd95236d0e8196b3
Tonko Mulder <tonko@tonkomulder.nl>
2019-10-19 10:28:35 +0200

    added model/migration/factory for user preferences
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

---
+++ database/migrations/2019_10_19_080558_create_user_preferences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserPreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_preferences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->tinyInteger('language')->default(2);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_preferences');
+    }
+}

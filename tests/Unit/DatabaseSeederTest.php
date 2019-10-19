0193a819b79ce6fa73c352c2afe2c01ec1a85d71
Matt Stauffer <matt@tighten.co>
2019-10-18 23:17:18 -0400

    Clean up database seeder test

--- tests/Unit/DatabaseSeederTest.php
+++ tests/Unit/DatabaseSeederTest.php
@@ -2,16 +2,28 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
+use FreshInstallSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use ReplaceOnlyContentFromSeedersSeeder;
+use Tests\TestCase;
 
 class DatabaseSeederTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function testBasicTest()
+    /** @test */
+    function fresh_install_seeder_runs()
     {
-        $this->seed(\DatabaseSeeder::class);
+        $this->seed(FreshInstallSeeder::class);
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    function old_install_seeder_runs_after_fresh()
+    {
+        $this->markTestIncomplete('Need to figure out how to run a test when the seeder asks for user input');
+        $this->seed(FreshInstallSeeder::class);
+        $this->seed(ReplaceOnlyContentFromSeedersSeeder::class);
         $this->assertTrue(true);
     }
 }

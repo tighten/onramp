928335ac60cc8f86e20e09a06ee7aba80525f01d
Jon Sugar <jonathansugar@mac.com>
2019-10-22 18:56:27 +0100

    Fix formatting.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,5 @@
 {{-- Template from https://templates.digizu.co.uk/ --}}<!DOCTYPE html>
-@php
-    $fullPageTitle = (isset($pageTitle) ? "{$pageTitle} | " : '') .  __('Onramp to Laravel');
-@endphp
+@php $fullPageTitle = (isset($pageTitle) ? "{$pageTitle} | " : '') .  __('Onramp to Laravel'); @endphp
 <html lang="{{ locale() }}">
     <head>
         <meta charset="utf-8">

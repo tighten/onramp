543b89f7a15cf309c5994ff68927dafd8f6c842e
Jon Sugar <jonathansugar@mac.com>
2019-10-22 12:01:54 +0100

    Add related resources to a term so that the related resource can be displayed on the glossary page.

--- app/Term.php
+++ app/Term.php
@@ -17,4 +17,23 @@ class Term extends Model
     {
         return $this->getTranslation('name', 'en');
     }
+
+    public function resources()
+    {
+        return $this->belongsToMany(Resource::class);
+    }
+
+    function resourcesForUser()
+    {
+        // get the user's language and preference, and only select appropriate resources
+        // @todo make this actually function
+        // $language = locale();
+        // $preference = enum('only current', 'only english', 'english and current')
+        $preference = 'only-current'; // @todo pull from user's preferences or whatever
+
+        switch ($preference) {
+            case 'only-current':
+                return $this->resources()->where('language', locale())->get();
+        }
+    }
 }

4be03d4bb255cae2948c88fdfb5dac745fe1fb49
Matt Stauffer <matt@tighten.co>
2019-10-26 17:13:30 -0400

    Clean up some views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,12 @@
 
 @section('content')
 <div class="w-full bg-white">
-    <!-- title -->
     <div class="text-center px-6 py-12 mb-6 bg-gray-100 border-b">
-        <h1 class=" text-xl md:text-4xl pb-4">My Onramp</h1>
+        <h1 class=" text-xl md:text-4xl pb-4">{{ __('My Onramp') }}</h1>
         <p class="leading-loose text-gray-dark">
             Tracking your progress
         </p>
     </div>
-    <!-- /title -->
 
     <div class="container max-w-4xl mx-auto md:flex items-start py-8 px-6 md:px-0">
         <div class="w-full md:pr-12 mb-6">

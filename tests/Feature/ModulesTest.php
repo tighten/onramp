c0c813539794a68b8047ccce0cf02abfafbc1a11
Tobias Etzold <info@etzold-online.de>
2019-10-27 12:52:49 +0100

    Test if module lists only contain bonus or standard modules

--- tests/Feature/ModulesTest.php
+++ tests/Feature/ModulesTest.php
@@ -31,4 +31,44 @@ class ModulesTest extends TestCase
             $bonusModule->name,
         ]);
     }
+
+    /** @test */
+    function list_of_standard_modules_only_contains_standard_modules()
+    {
+        $standardModule = factory(Module::class)->create([
+            'name' => 'Standard module',
+            'is_bonus' => 0,
+        ]);
+
+        $bonusModule = factory(Module::class)->create([
+            'name' => 'Bonus module',
+            'is_bonus' => 1,
+        ]);
+
+        $response = $this->get("/en/modules");
+
+        $this->assertTrue(count($this->getResponseData($response, 'standardModules')) == 1);
+        $this->assertTrue($this->getResponseData($response, 'standardModules')[0]->name == $standardModule->name);
+        $this->assertFalse($this->getResponseData($response, 'standardModules')[0]->name == $bonusModule->name);
+    }
+
+    /** @test */
+    function list_of_bonus_modules_only_contains_bonus_modules()
+    {
+        $standardModule = factory(Module::class)->create([
+            'name' => 'Standard module',
+            'is_bonus' => 0,
+        ]);
+
+        $bonusModule = factory(Module::class)->create([
+            'name' => 'Bonus module',
+            'is_bonus' => 1,
+        ]);
+
+        $response = $this->get("/en/modules");
+
+        $this->assertTrue(count($this->getResponseData($response, 'bonusModules')) == 1);
+        $this->assertTrue($this->getResponseData($response, 'bonusModules')[0]->name == $bonusModule->name);
+        $this->assertFalse($this->getResponseData($response, 'bonusModules')[0]->name == $standardModule->name);
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    function getResponseData($response, $key){
+        return $response->getOriginalContent()->getData()[$key]->all();
+    }
 }

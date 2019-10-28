ac04a0012a96e59eb78cfa2493037d3b5a62cb5a
Matt Stauffer <matt@tighten.co>
2019-10-27 23:26:42 -0400

    Fix Vue test, leave note about Dusk

--- tests/Feature/PreferenceRoutesTest.php
+++ tests/Feature/PreferenceRoutesTest.php
@@ -12,13 +12,15 @@ class PreferenceRoutesTest extends TestCase
 {
     use RefreshDatabase;
 
+    // @todo: Consider Dusk test here since this magic string can get out of
+    // sync with the Vue and then it's not all that useful
     /** @test */
     function valid_preferences_posted_are_persisted()
     {
         $user = factory(User::class)->create();
         $this->be($user);
         $response = $this->post(route_wlocale('user.preferences.store'), [
-            'language_preference' => 'qwerty'
+            'resource-language-preference' => 'qwerty'
         ]);
 
         $this->assertEquals('qwerty', Preferences::get('resource-language-preference'));

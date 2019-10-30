46d430a710fba87b274603f583c7c233a4015eac
Matt Stauffer <matt@tighten.co>
2019-10-30 09:47:09 -0400

    Add first enum for resource types

---
+++ app/ResourceType.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+class ResourceType extends Enum
+{
+    const VIDEO = 'video';
+    const COURSE = 'course';
+    const AUDIO = 'audio';
+    const BOOK = 'book';
+    const ARTICLE = 'article';
+}

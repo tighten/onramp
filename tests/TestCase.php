d32b21beef8b2b4b5d37ae6f879eb63eb535339f
Tobias Etzold <info@etzold-online.de>
2019-10-30 21:05:50 +0100

    Changed custom assertion to assertViewHas()

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,8 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    function getResponseData($response, $key){
-        return $response->getOriginalContent()->getData()[$key]->all();
-    }
 }

33141547415c353a3ebc092f93edb5b4c97ecba9
Marisa Clardy <marisa@clardy.eu>
2019-10-30 23:39:46 -0500

    Use the new Operating System class.

--- app/Http/Controllers/Auth/WizardController.php
+++ app/Http/Controllers/Auth/WizardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Localization\Locale;
+use App\OperatingSystem;
 use Illuminate\Validation\Rule;
 
 class WizardController extends Controller
@@ -13,17 +14,19 @@ class WizardController extends Controller
     public function __invoke()
     {
         $valid = $this->validate(request(), [
-            'os' => ['required', 'string', 'in:any,osx,linux,windows'],
-            'track' => ['required', 'int', 'exists:tracks,id', 'min:1'],
+            'os' => ['required', 'string', Rule::in(OperatingSystem::keys())],
+            'track' => ['required', 'int', 'exists:tracks,id'],
             'locale' => ['required', 'string', Rule::in((new Locale())->slugs())]
         ], [], [
             'os' => 'OS'
         ]);
-        $user = auth()->user();
-        $user->os = $valid['os'];
-        $user->track_id = $valid['track'];
-        // TODO: Add language save. Waiting on PR#103.
-        $user->save();
+
+        auth()->user()->update([
+            'os' => $valid['os'],
+            'track_id' => $valid['track']
+        ]);
+        // @todo Add language save. Waiting on PR#103.
+
         return redirect(path_wlocale($this->redirectTo));
     }
 }

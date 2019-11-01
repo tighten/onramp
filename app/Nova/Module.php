837e1e51662daedd8312eb802b23baa3714bc034
Matt Stauffer <matt@tighten.co>
2019-11-01 12:00:05 -0400

    Relate tracks and modules in Nova; fix track->module relationship

--- app/Nova/Module.php
+++ app/Nova/Module.php
@@ -3,6 +3,7 @@
 namespace App\Nova;
 
 use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
@@ -52,6 +53,8 @@ class Module extends Resource
             Text::make('Slug')
                 ->sortable()
                 ->rules('required', 'max:255'),
+
+            BelongsToMany::make('Tracks'),
         ];
     }
 

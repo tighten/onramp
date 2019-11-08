d0dddcf4f87fefe4c41e7938729101179e8194ed
Matt Stauffer <matt@tighten.co>
2019-11-08 12:12:51 -0500

    Add Term Resource

---
+++ app/Nova/Term.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\ID;
+use MrMonat\Translatable\Translatable;
+
+class Term extends Resource
+{
+    public static $model = \App\Term::class;
+
+    public static $title = 'name';
+
+    public static $search = [
+        'id', 'name', 'description'
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make()->sortable(),
+
+            Translatable::make('Name')
+                ->singleLine()
+                ->indexLocale('en'),
+
+            Translatable::make('Description')
+                ->hideFromIndex()
+                ->indexLocale('en'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [];
+    }
+}

4025ae8f3a70d7fa9d83bf52bddc59aeac86601e
Matt Stauffer <matt@tighten.co>
2019-11-08 11:45:58 -0500

    Add role to users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 use App\Track;
 use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
@@ -12,6 +13,7 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'role' => Arr::random(['admin', 'editor', 'user']),
         'track_id' => function () use ($faker) {
             if (! $faker->boolean) {
                 return null;

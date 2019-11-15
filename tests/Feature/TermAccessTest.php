dbcd8eaa13a499184834fe7bd2c793f7c925eded
Matt Stauffer <matt@tighten.co>
2019-11-15 11:17:24 -0500

    Add policy and access test for terms

---
+++ tests/Feature/TermAccessTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Term;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TermAccessTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function users_with_user_role_cannot_update_terms()
+    {
+        $user = factory(User::class)->create(['role' => 'user']);
+
+        $this->assertFalse($user->can('update', Term::class));
+    }
+
+    /** @test */
+    function users_with_editor_role_can_update_terms()
+    {
+        $user = factory(User::class)->create(['role' => 'editor']);
+
+        $this->assertTrue($user->can('update', Term::class));
+    }
+
+    /** @test */
+    function users_with_admin_role_can_update_terms()
+    {
+        $user = factory(User::class)->create(['role' => 'admin']);
+
+        $this->assertTrue($user->can('update', Term::class));
+    }
+}

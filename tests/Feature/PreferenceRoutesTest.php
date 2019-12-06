4ec74c5f8ea9992918d3c37f455f01f4763dd341
Matt Stauffer <matt@tighten.co>
2019-12-06 11:02:54 -0500

    Test Preferences routes

--- tests/Feature/PreferenceRoutesTest.php
+++ tests/Feature/PreferenceRoutesTest.php
@@ -14,10 +14,11 @@ class PreferenceRoutesTest extends TestCase
     /** @test */
     function resource_language_can_be_changed()
     {
-        $user = factory(User::class)->create();
-        $this->be($user);
+        $this->be(factory(User::class)->create());
         $response = $this->post(route_wlocale('user.preferences.store'), [
             'resource-language' => 'local-and-english',
+            'locale' => 'en',
+            'operating-system' => 'macos',
         ]);
 
         $this->assertEquals('local-and-english', Preferences::get('resource-language'));
@@ -26,10 +27,11 @@ class PreferenceRoutesTest extends TestCase
     /** @test */
     function locale_can_be_changed()
     {
-        $user = factory(User::class)->create();
-        $this->be($user);
+        $this->be(factory(User::class)->create());
         $response = $this->post(route_wlocale('user.preferences.store'), [
             'locale' => 'es',
+            'resource-language' => 'local-and-english',
+            'operating-system' => 'macos',
         ]);
 
         $this->assertEquals('es', Preferences::get('locale'));
@@ -38,10 +40,11 @@ class PreferenceRoutesTest extends TestCase
     /** @test */
     function operating_system_can_be_changed()
     {
-        $user = factory(User::class)->create();
-        $this->be($user);
+        $this->be(factory(User::class)->create());
         $response = $this->post(route_wlocale('user.preferences.store'), [
             'operating-system' => 'linux',
+            'locale' => 'es',
+            'resource-language' => 'local-and-english',
         ]);
 
         $this->assertEquals('linux', Preferences::get('operating-system'));

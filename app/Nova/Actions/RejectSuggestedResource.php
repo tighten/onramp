10a1b0780fb0472239632fe1a0ccdbfc70c76e8c
TammyTee <TammyTee@users.noreply.github.com>
2020-02-28 17:32:16 -0500

    - add status and rejected reason to suggested resources table - create status filter for suggested resources

---
+++ app/Nova/Actions/RejectSuggestedResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Nova\Actions;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Collection;
+use Laravel\Nova\Actions\Action;
+use Laravel\Nova\Fields\ActionFields;
+
+class RejectSuggestedResource extends Action
+{
+    use InteractsWithQueue, Queueable;
+
+    /**
+     * Perform the action on the given models.
+     *
+     * @param  \Laravel\Nova\Fields\ActionFields  $fields
+     * @param  \Illuminate\Support\Collection  $models
+     * @return mixed
+     */
+    public function handle(ActionFields $fields, Collection $models)
+    {
+        //
+    }
+
+    /**
+     * Get the fields available on the action.
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return [];
+    }
+}

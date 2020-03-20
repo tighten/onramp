570d7ea015ecba1f67342656a7b3ac8e5a84a1ce
TammyTee <TammyTee@users.noreply.github.com>
2020-03-20 17:09:31 -0400

    Add boolean field to Resources in Nova to quickly see if a resource has a module
    With the updated relationship between resources and modules, you can no longer assign a module when manually creating a resource. This ensures admins and editors are getting resources assigned to their appropiate modules.

--- app/Nova/Resource.php
+++ app/Nova/Resource.php
@@ -6,6 +6,7 @@ use App\Facades\Localization;
 use App\Resource as EloquentResource;
 use Illuminate\Http\Request;
 use Inspheric\Fields\Url;
+use Laravel\Nova\Fields\Badge;
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Fields\BelongsToMany;
 use Laravel\Nova\Fields\Boolean;
@@ -71,9 +72,15 @@ class Resource extends BaseResource
                 ->options(array_merge(['all' => 'All (contains multiple translations)'], Localization::all()))
                 ->rules('required'),
 
-            Boolean::make('Is Free'),
+            Boolean::make('Is Free')
+                ->hideFromIndex(),
 
-            Boolean::make('Is Bonus'),
+            Boolean::make('Is Bonus')
+                ->hideFromIndex(),
+
+            Boolean::make('Is Assigned To Module', function () {
+                return $this->isAssignedToAModule();
+            }),
 
             BelongsToMany::make('Modules'),
         ];

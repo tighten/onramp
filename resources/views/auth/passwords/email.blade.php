a224abd58635d96241011c5da7c1fe3bff12f642
TammyTee <TammyTee@users.noreply.github.com>
2020-04-28 12:01:46 -0400

    update design for auth views

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,53 +1,85 @@
-@extends('layouts.app')
+@extends('layouts.app-without-nav')
 
 @section('content')
-    <div class="container mx-auto mt-8 mb-8">
-        <div class="flex flex-wrap justify-center">
-            <div class="w-full max-w-sm">
+<div class="flex flex-col justify-center min-h-screen py-12 bg-off-white sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <a href="{{ route_wlocale('welcome') }}">
+            <img class="w-auto h-20 mx-auto" src="/images/logo/onramp-mark.svg" alt="Onramp" />
+        </a>
+    </div>
 
-                @if (session('status'))
-                    <div class="px-3 py-4 mb-4 text-sm text-green-700 bg-green-100 border border-t-8 border-green-600 rounded" role="alert">
-                        {{ session('status') }}
+    @if (session('status'))
+        <div class="p-4 mt-8 bg-green-100 rounded-md sm:mx-auto sm:w-full sm:max-w-md" role="alert">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <svg class="w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                    </svg>
+                </div>
+
+                <div class="ml-3">
+                    <h3 class="text-sm font-semibold leading-5 text-green-900">
+                        Password Reset Link Sent
+                    </h3>
+
+                    <div class="mt-2 text-sm leading-5 text-green-700">
+                        <p>{{ session('status') }}</p>
                     </div>
-                @endif
-
-                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-
-                    <div class="px-6 py-3 mb-0 font-semibold text-gray-700 bg-gray-200">
-                        {{ __('Reset Password') }}
-                    </div>
-
-                    <form class="w-full p-6" method="POST" action="{{ route_wlocale('password.email') }}">
-                        @csrf
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="email" class="block mb-2 text-sm font-bold text-gray-700">
-                                {{ __('E-Mail Address') }}:
-                            </label>
-
-                            <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline{{ $errors->has('email') ? ' border-red' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                            @if ($errors->has('email'))
-                                <p class="mt-2 text-xs italic text-red-500">
-                                    {{ $errors->first('email') }}
-                                </p>
-                            @endif
-                        </div>
-
-                        <div class="flex flex-wrap">
-                            <button type="submit" class="px-4 py-2 font-bold text-gray-100 bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">
-                                {{ __('Send Password Reset Link') }}
-                            </button>
-
-                            <p class="w-full mt-8 -mb-4 text-xs text-center text-grey-dark">
-                                <a class="text-blue-500 no-underline hover:text-blue-700" href="{{ route_wlocale('login') }}">
-                                    {{ __("Back to login") }}
-                                </a>
-                            </p>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
+    @endif
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+            <form method="POST" action="{{ route_wlocale('password.email') }}">
+                @csrf
+                <h2 class="mb-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
+                    {{ __('Reset Password') }}
+                </h2>
+
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-5 text-gray-700">
+                        {{ __('Email address') }}
+                    </label>
+
+                    <div class="mt-1 rounded-md shadow-sm">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            value="{{ old('email') }}"
+                            required
+                            autofocus
+                            class="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5{{ $errors->has('email') ? ' border-red-500' : '' }}" />
+                    </div>
+
+                    @if ($errors->has('email'))
+                        <p class="mt-2 text-xs italic text-red-500">{{ $errors->first('email') }}</p>
+                    @endif
+                </div>
+
+                <div class="mt-6">
+                    <span class="block w-full rounded-md shadow-sm">
+                        <button
+                            type="submit"
+                            class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-teal-600 border border-transparent rounded-md hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700">
+                            {{ __('Send Password Reset Link') }}
+                        </button>
+                    </span>
+                </div>
+
+                <div class="mt-6 text-center">
+                    <div class="text-sm leading-5">
+                        <a
+                            href="{{ route_wlocale('login') }}"
+                            class="font-medium text-teal-600 transition duration-150 ease-in-out hover:text-teal-500 focus:outline-none focus:underline">
+                            {{ __('Back to login') }}
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
+</div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,89 @@
-@extends('layouts.app')
+@extends('layouts.app-without-nav')
 
 @section('content')
-    <div class="container mx-auto mt-8 mb-8">
-        <div class="flex flex-wrap justify-center">
-            <div class="w-full max-w-sm">
-                <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+<div class="flex flex-col justify-center min-h-screen py-12 bg-off-white sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+        <a href="{{ route_wlocale('welcome') }}">
+            <img class="w-auto h-20 mx-auto" src="/images/logo/onramp-mark.svg" alt="Onramp" />
+        </a>
+    </div>
 
-                    <div class="px-6 py-3 mb-0 font-semibold text-gray-700 bg-gray-200">
-                        {{ __('Reset Password') }}
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div class="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+            <form method="POST" action="{{ route_wlocale('password.update') }}">
+                @csrf
+
+                <input type="hidden" name="token" value="{{ $token }}">
+
+                <h2 class="mb-6 text-3xl font-extrabold leading-9 text-center text-gray-900">
+                    {{ __('Reset Password') }}
+                </h2>
+
+                <div>
+                    <label for="email" class="block text-sm font-medium leading-5 text-gray-700">
+                        {{ __('Email address') }}
+                    </label>
+
+                    <div class="mt-1 rounded-md shadow-sm">
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            value="{{ old('email') }}"
+                            required
+                            class="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5{{ $errors->has('email') ? ' border-red-500' : '' }}" />
                     </div>
 
-                    <form class="w-full p-6" method="POST" action="{{ route_wlocale('password.update') }}">
-                        @csrf
-
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="email" class="block mb-2 text-sm font-bold text-gray-700">
-                                {{ __('E-Mail Address') }}:
-                            </label>
-
-                            <input id="email" type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline{{ $errors->has('email') ? ' border-red' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                            @if ($errors->has('email'))
-                                <p class="mt-2 text-xs italic text-red-500">
-                                    {{ $errors->first('email') }}
-                                </p>
-                            @endif
-                        </div>
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="password" class="block mb-2 text-sm font-bold text-gray-700">
-                                {{ __('Password') }}:
-                            </label>
-
-                            <input id="password" type="password" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline{{ $errors->has('password') ? ' border-red' : '' }}" name="password" required>
-
-                            @if ($errors->has('password'))
-                                <p class="mt-2 text-xs italic text-red-500">
-                                    {{ $errors->first('password') }}
-                                </p>
-                            @endif
-                        </div>
-
-                        <div class="flex flex-wrap mb-6">
-                            <label for="password-confirm" class="block mb-2 text-sm font-bold text-gray-700">
-                                {{ __('Confirm Password') }}:
-                            </label>
-
-                            <input id="password-confirm" type="password" class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline" name="password_confirmation" required>
-                        </div>
-
-                        <div class="flex flex-wrap">
-                            <button type="submit" class="px-4 py-2 font-bold text-gray-100 bg-blue-500 rounded hover:bg-blue-700  focus:outline-none focus:shadow-outline">
-                                {{ __('Reset Password') }}
-                            </button>
-                        </div>
-                    </form>
-
+                    @if ($errors->has('email'))
+                        <p class="mt-2 text-xs italic text-red-500">{{ $errors->first('email') }}</p>
+                    @endif
                 </div>
-            </div>
+
+                <div class="mt-6">
+                    <label for="password" class="block text-sm font-medium leading-5 text-gray-700">
+                        {{ __('Password') }}
+                    </label>
+
+                    <div class="mt-1 rounded-md shadow-sm">
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            required
+                            class="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5{{ $errors->has('password') ? ' border-red-500' : '' }}" />
+                    </div>
+
+                    @if ($errors->has('password'))
+                        <p class="mt-2 text-xs italic text-red-500">{{ $errors->first('password') }}</p>
+                    @endif
+                </div>
+
+                <div class="mt-6">
+                    <label for="password-confirm" class="block text-sm font-medium leading-5 text-gray-700">
+                        {{ __('Confirm Password') }}
+                    </label>
+
+                    <div class="mt-1 rounded-md shadow-sm">
+                        <input
+                            id="password-confirm"
+                            name="password_confirmation"
+                            type="password"
+                            required
+                            class="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5" />
+                    </div>
+                </div>
+
+                <div class="mt-6">
+                    <span class="block w-full rounded-md shadow-sm">
+                        <button
+                            type="submit"
+                            class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-teal-600 border border-transparent rounded-md hover:bg-teal-500 focus:outline-none focus:border-teal-700 focus:shadow-outline-teal active:bg-teal-700">
+                            {{ __('Reset password') }}
+                        </button>
+                    </span>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 @endsection

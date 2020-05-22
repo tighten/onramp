c89f8c26bfda5f030d6fe612582ce3cd74fd93c7
Keith Damiani <keith@tighten.co>
2020-05-22 14:04:45 -0400

    Create translations.js file, import to app.js, update config

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,19 +8,6 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/LanguageSwitcher.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-// Vue.component('language-switcher', require('./components/LanguageSwitcher.vue').default);
-
 import LanguageSwitcher from './components/LanguageSwitcher.vue';
 import ToggleWhenMobile from './components/ToggleWhenMobile.vue';
 import ResourceLanguagePreferenceSwitcher from './components/ResourceLanguagePreferenceSwitcher.vue';
@@ -28,20 +15,15 @@ import CompletedCheckbox from './components/CompletedCheckbox.vue';
 import Toast from './components/Toast.vue';
 import Lang from 'lang.js';
 import Notifications from 'vue-notification';
+import Translations from './translations';
 
 Vue.prototype.trans = new Lang({
-    messages: window.jsonTranslations,
+    messages: Translations,
     locale: window.locale,
     fallback: window.fallback_locale
 });
 
 Vue.use(Notifications);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     components: {

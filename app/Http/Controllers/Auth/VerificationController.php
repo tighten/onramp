f7371aa26c9d85dff088cddd322cd2098aeea6b7
TammyTee <TammyTee@users.noreply.github.com>
2020-06-26 12:57:35 -0400

    Update auth verification redirect url

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -25,7 +25,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/en/home';
+    protected $redirectTo = '/en/modules';
 
     /**
      * Create a new controller instance.

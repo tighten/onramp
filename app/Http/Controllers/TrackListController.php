e948c9044cfefddc06bd5f4679d257442c9c8409
Alison <alison@dflydev.com>
2020-09-21 13:21:12 -0500

    Add Tracks page

---
+++ app/Http/Controllers/TrackListController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Module;
+use App\Track;
+
+class TrackListController extends Controller
+{
+    public function index()
+    {
+        $tracks = Track::all();
+        $modulesWithTracks = Module::with('tracks')->get();
+
+        $modules = $modulesWithTracks->reduce(function ($carry, $row) use ($tracks) {
+            $hasTracks = $tracks->reduce(function ($carry, $track) use ($row) {
+                $carry[$track->name] = $row->tracks->pluck('name')->contains($track->name);
+                return $carry;
+            }, []);
+
+            $carry[$row->name] = [
+                'module_name' => $row->name,
+                'has_tracks' => $hasTracks,
+            ];
+
+            return $carry;
+        }, []);
+
+        return view('tracks', [
+            'modules' => $modules,
+            'tracks' => $tracks->pluck('name'),
+        ]);
+    }
+}

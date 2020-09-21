7ca039c7971058e0e084c47527172b008159683e
Matt Stauffer <matt@tighten.co>
2020-09-21 18:14:09 -0400

    Move logic to the view

--- app/Http/Controllers/TrackListController.php
+++ app/Http/Controllers/TrackListController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Module;
 use App\Track;
-use Illuminate\Database\Eloquent\Collection;
 
 class TrackListController extends Controller
 {
@@ -13,29 +11,8 @@ class TrackListController extends Controller
         $tracks = Track::has('modules')->with('modules')->get();
 
         return view('tracks', [
-            'modules' => $this->tracksForTable($tracks),
             'tracks' => $tracks,
+            'modules' => $tracks->flatMap->modules->unique->id->sort(),
         ]);
-    }
-
-    public function tracksForTable(Collection $tracks)
-    {
-        return $tracks->flatMap(function ($track) {
-            return $track->modules;
-
-            // @todo de-duplicate modules
-        })->mapWithKeys(function ($module) use ($tracks) {
-            return [$module->name => $this->moduleForTable($module, $tracks)];
-        })->sort()->toArray();
-    }
-
-    public function moduleForTable(Module $module, $tracks)
-    {
-        return  [
-            'module_name' => $module->name,
-            'has_tracks' => $tracks->mapWithKeys(function ($track) use ($module) {
-                return [$track->name => $track->modules->pluck('id')->contains($module->id)];
-            })->toArray(),
-        ];
     }
 }

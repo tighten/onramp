aa4b061292533b89c476137ae59df43569b2729c
Laravel Shift <shift@laravelshift.com>
2021-05-14 15:44:31 +0000

    Adopt Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-2 coding style as a base with some minor additions.
    
    You may customize the adopted coding style by adding your own [PHP CS Fixer][1] `.php_cs` config file to your project root. Feel free to use [Shift's Laravel ruleset][2] to help you get started.
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200

--- database/factories/SuggestedResourceFactory.php
+++ database/factories/SuggestedResourceFactory.php
@@ -2,9 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Facades\Localization;
 use App\SuggestedResource;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Facades\Localization;
 
 class SuggestedResourceFactory extends Factory
 {

0ac9cc068ad81c6cb13e4cc98742deb38c1f3abe
Marcy Acevedo <marcy.acevedo@tighten.co>
2021-11-19 16:02:36 -0600

    Start refactoring mobile nav to use alpine.

--- resources/views/partials/navigation/header/main-nav.blade.php
+++ resources/views/partials/navigation/header/main-nav.blade.php
@@ -1,18 +1,17 @@
-<nav class="flex items-center">
-	<a class="block mx-1 text-body font-semibold transition-colors duration-300 ease-in-out text-mint hover:text-white hover:no-underline px-3 py-1
-		{{ Route::currentRouteName() === 'modules.index' ? 'border-2 border-mint rounded-3xl' : '' }}"
-		href="{{ route_wlocale('modules.index') }}">
-		<span>{{ __('Learn') }}</span>
-	</a>
+<a class="block mx-1 border-t border-silver lg:border-none text-body font-bold transition-colors py-4 lg:py-1 duration-300 ease-in-out text-mint hover:text-white hover:no-underline px-3
+	{{ Route::currentRouteName() === 'modules.index' ? 'lg:border-2 lg:border-mint lg:rounded-3xl' : '' }}"
+    href="{{ route_wlocale('modules.index') }}">
+    <span>{{ __('Learn') }}</span>
+</a>
 
-	<a class="block mx-1 text-body font-bold transition-colors duration-300 ease-in-out text-mint hover:text-white hover:no-underline px-3 py-1
-		{{ Route::currentRouteName() === 'glossary' ? 'border-2 border-mint rounded-3xl' : '' }}"
-		href="{{ route_wlocale('glossary') }}">
-		<span>{{ __('Glossary') }}</span>
-	</a>
-	<a class="block mx-1 text-body font-bold transition-colors duration-300 ease-in-out text-mint hover:text-white hover:no-underline px-3 py-1
-		{{ Route::currentRouteName() === 'tracks' ? 'border-2 border-mint rounded-3xl' : '' }}"
-		href="{{ route_wlocale('tracks') }}">
-		<span>{{ __('Tracks') }}</span>
-	</a>
-</nav>
+<a class="block mx-1 border-t border-silver lg:border-none text-body font-bold transition-colors py-4 lg:py-1 duration-300 ease-in-out text-mint hover:text-white hover:no-underline px-3
+	{{ Route::currentRouteName() === 'glossary' ? 'lg:border-2 lg:border-mint lg:rounded-3xl' : '' }}"
+    href="{{ route_wlocale('glossary') }}">
+    <span>{{ __('Glossary') }}</span>
+</a>
+
+<a class="block mx-1 border-t border-silver lg:border-none text-body font-bold transition-colors py-4 lg:py-1 duration-300 ease-in-out text-mint hover:text-white hover:no-underline px-3
+	{{ Route::currentRouteName() === 'tracks' ? 'lg:border-2 lg:border-mint lg:rounded-3xl' : '' }}"
+    href="{{ route_wlocale('tracks') }}">
+    <span>{{ __('Tracks') }}</span>
+</a>
--- resources/views/partials/navigation/header/mobile-header.blade.php
+++ resources/views/partials/navigation/header/mobile-header.blade.php
@@ -1,79 +1,88 @@
-<modal-mobile-menu :show="modals.mobileMenu">
-    <template slot="navigation-links">
-        <a class="block p-6 font-bold border-t text-body border-silver text-mint hover:no-underline"
-            href="{{ route_wlocale('modules.index') }}">
-            <span>{{ __('Learn') }}</span>
-        </a>
+<section x-data="{ open: false }">
+    <button class="relative mr-4 focus:outline-none lg:hidden"
+        aria-label="open menu"
+        x-on:click="open = true">
+        <div class="h-[2px] my-3 rounded w-8 bg-mint mobileMenuBtn"></div>
+    </button>
 
-        <a class="block p-6 font-bold border-t text-body border-silver text-mint hover:no-underline"
-            href="{{ route_wlocale('glossary') }} ">
-            <span>{{ __('Glossary') }}</span>
-        </a>
+    <div x-show="open"
+        @click.away="open = false">
+        <div>
+            @include('partials.navigation.header.main-nav')
+        </div>
 
-        <a class="block p-6 font-bold border-t text-body border-silver text-mint hover:no-underline"
-            href="{{ route_wlocale('tracks') }} ">
-            <span>{{ __('Tracks') }}</span>
-        </a>
-    </template>
-
-    <template slot="subnavigation-links">
-        @auth
-            <a class="block px-6 py-3 text-base font-bold text-mint hover:no-underline"
-                href="{{ url_wlocale('preferences') }}">
-                <span>{{ __('Preferences') }}</span>
-            </a>
-        @endauth
-
-        @include('partials.language-switcher')
-    </template>
-
-    <template slot="navigation-buttons">
-        @guest
-            <a class="flex-1 inline-block px-4 py-1 mx-2 text-lg font-bold text-center transition duration-150 ease-in-out bg-transparent border-2 hover:no-underline rounded-3xl text-mint border-mint hover:bg-mint hover:text-blue-black"
-                href="{{ route_wlocale('login') }}">
-                <span>{{ __('Log in') }}</span>
-            </a>
-
-            @if (Route::has('register'))
-                <a class="flex-1 inline-block px-4 py-1 mx-2 text-lg font-bold text-center transition duration-150 ease-in-out border-2 under rounded-3xl border-mint bg-mint hover:no-underline hover:bg-transparent hover:text-mint"
-                    href="{{ route_wlocale('register') }}">
-                    <span>{{ __('Register') }}</span>
+        <div>
+            @auth
+                <a class="block px-6 pb-4 mb-3 font-bold border-b text-body text-mint hover:no-underline border-silver"
+                    href="{{ url_wlocale('preferences') }}">
+                    <span>{{ __('Preferences') }}</span>
                 </a>
+            @endauth
+
+            @include('partials.language-switcher')
+        </div>
+
+        <div>
+            @guest
+                <a class="flex-1 inline-block px-4 py-1 mx-2 text-lg font-bold text-center transition duration-150 ease-in-out bg-transparent border-2 hover:no-underline rounded-3xl text-mint border-mint hover:bg-mint hover:text-blue-black"
+                    href="{{ route_wlocale('login') }}">
+                    <span>{{ __('Log in') }}</span>
                 </a>
-            @endif
-        @else
-            <a href="{{ route_wlocale('logout') }}"
-                class="flex-1 inline-block w-1/2 py-3 mx-2 text-lg font-bold leading-none text-center text-white whitespace-no-wrap border-2 bg-mint border-mint hover:no-underline"
-                onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                <span>{{ __('Log out') }}</span>
-            </a>
-            <form id="logout-form"
-                action="{{ route_wlocale('logout') }}"
-                method="POST"
-                class="hidden">
-                {{ csrf_field() }}
-            </form>
-        @endguest
-    </template>
-</modal-mobile-menu>
 
+                @if (Route::has('register'))
+                    <a class="flex-1 inline-block px-4 py-1 mx-2 text-lg font-bold text-center transition duration-150 ease-in-out border-2 under rounded-3xl border-mint bg-mint hover:no-underline hover:bg-transparent hover:text-mint"
+                        href="{{ route_wlocale('register') }}">
+                        <span>{{ __('Register') }}</span>
+                    </a>
+                    </a>
+                @endif
+            @else
+                <a href="{{ route_wlocale('logout') }}"
+                    class="flex-1 inline-block w-1/2 py-3 mx-2 text-lg font-bold leading-none text-center text-white whitespace-no-wrap border-2 bg-mint border-mint hover:no-underline"
+                    onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    <span>{{ __('Log out') }}</span>
+                </a>
+                <form id="logout-form"
+                    action="{{ route_wlocale('logout') }}"
+                    method="POST"
+                    class="hidden">
+                    {{ csrf_field() }}
+                </form>
+            @endguest
+        </div>
+    </div>
+</section>
 
-
-<button class="relative mr-4 focus:outline-none lg:hidden"
-                aria-label="open menu"
-                @click="openModal('mobileMenu')">
-                <div class="h-[2px] my-3 rounded w-8 bg-mint mobileMenuBtn">
-                </div>
-            </button>
-            {{-- <button @click="openModal('mobileMenu')
-                aria-label="
-                open
-                menu"
+{{-- <div x-data="{ open: false }"
+    x-description="Mobile menu, toggle classes based on menu state."
+    class="relative w-full py-4 text-center text-white transition ease-in opacity-0 e duration-400 lg:hidden h-75">
+    <div class="flex items-center justify-between px-4">
+        <div class="flex items-center">
+            <button x-on:click="open = true"
+                aria-label="Main Menu"
                 type="button"
-                class="relative mr-4 mobileMenuBtn focus:outline-none lg:hidden"
-                :class="openModal('mobileMenu') ? 'isActive' : ''">
-                <div class="h-[2px] my-3 rounded w-8 bg-mint"
-                    :class="openModal('mobileMenu') ? 'invisible' : 'visible'">
-                </div>
-            </button> --}}
+                class="relative mr-4 text-white mobileMenuBtn"
+                x-bind:class="open ?'isActive' : ''">
+                button!!!!
+            </button>
+
+            <div class="fixed z-[-1] top-0 left-0 flex items-center justify-center w-full  bg-black bg-opacity-50"
+                x-show="open"></div>
+
+
+        </div>
+
+        <div class="flex items-center text-navy">
+            <div class="mr-px">
+            </div>
+
+        </div>
+    </div>
+    <div x-show="open">
+
+        wweeeeyyyooo
+
+
+    </div>
+</div> --}}

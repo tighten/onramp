1384015ebe0a1d42a693936813f74fd74d46070d
Marcy Acevedo <marcy.acevedo@tighten.co>
2021-11-19 16:02:36 -0600

    Start refactoring mobile nav to use alpine.

--- resources/views/partials/navigation/header/desktop-header.blade.php
+++ resources/views/partials/navigation/header/desktop-header.blade.php
@@ -8,26 +8,17 @@
     @include('partials.navigation.header.logo')
     <div class="items-center justify-between flex-1 hidden pl-12 lg:flex">
         <div class="flex-1">
-            @include('partials.navigation.header.main-nav')
+            <nav class="flex items-center">
+                @include('partials.navigation.header.main-nav')
+            </nav>
         </div>
 
         <div class="flex items-center justify-end flex-1">
-
             @include('partials.language-switcher')
 
             <div class="flex items-center ml-12">
                 @guest
-                    <a class="flex-1 inline-block px-4 py-1 mx-2 text-lg font-semibold text-center transition duration-150 ease-in-out bg-transparent border-2 hover:no-underline rounded-3xl text-mint border-mint hover:bg-mint hover:text-blue-black"
-                        href="{{ route_wlocale('login') }}">
-                        <span>{{ __('Log in') }}</span>
-                    </a>
-
-                    @if (Route::has('register'))
-                        <a class="flex-1 inline-block px-4 py-1 mx-2 text-lg font-semibold text-center transition duration-150 ease-in-out border-2 under rounded-3xl border-mint bg-mint hover:no-underline hover:bg-transparent hover:text-mint"
-                            href="{{ route_wlocale('register') }}">
-                            <span>{{ __('Register') }}</span>
-                        </a>
-                    @endif
+                    @include('partials.navigation.header.guest-menu')
                 @else
                     @include('partials.navigation.header.auth-menu')
 

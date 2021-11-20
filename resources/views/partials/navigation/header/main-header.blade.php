74b8ca462c0f6d029b0efd5d15cd569a8a94a1ad
Marcy Acevedo <marcy.acevedo@tighten.co>
2021-11-19 20:22:36 -0600

    Start on welcome page hero

--- resources/views/partials/navigation/header/main-header.blade.php
+++ resources/views/partials/navigation/header/main-header.blade.php
@@ -1,4 +1,4 @@
-<section class="w-full py-5 border-t-4 bg-blue-black border-violet lg:border-t-8 z-[9999] fixed left-0 top-0">
+<section class="w-full py-5 border-t-4 bg-blue-black border-violet lg:border-t-8 z-[9999]">
     @include('partials.navigation.header.desktop-header')
     @include('partials.navigation.header.mobile-header')
 </section>

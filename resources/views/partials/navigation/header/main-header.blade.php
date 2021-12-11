8a9e9adeb474dcc3c33d3c500b7bd578c8a818de
Marcy Acevedo <marcy.acevedo@tighten.co>
2021-12-10 19:19:34 -0600

    Style finessing. Finish home page.

--- resources/views/partials/navigation/header/main-header.blade.php
+++ resources/views/partials/navigation/header/main-header.blade.php
@@ -1,4 +1,4 @@
-<section class="w-full py-5 border-t-4 bg-blue-black border-violet lg:border-t-8">
+<section class="w-full pt-5 pb-10 border-t-4 bg-gradient-to-b from-blue-black via-blue-black to-transparent border-violet lg:border-t-8">
     @include('partials.navigation.header.desktop-header')
     @include('partials.navigation.header.mobile-header')
 </section>
--- resources/views/partials/welcome-page/demographic.blade.php
+++ resources/views/partials/welcome-page/demographic.blade.php
@@ -4,30 +4,30 @@
 	</p>
 
 	<div class="px-4 mlg:px-0 max-w-[1100px] mx-auto relative gap-4 grid grid-cols-1 pt-6 mt-8 sm:grid-cols-3">
-		<div class="p-1 rounded-md bg-gradient-to-r from-teal via-teal to-violet">
+		<div class="p-1 rounded-md bg-gradient-to-r from-mint via-mint to-violet">
 			<div class="py-16 text-2xl rounded-md bg-blue-black lg:h4">
 				<div
-					class="flex items-center justify-center w-16 h-16 mx-auto rounded-full bg-teal bg-teal-dark md:w-20 md:h-20">
+					class="flex items-center justify-center w-16 h-16 mx-auto rounded-full bg-mint md:w-20 md:h-20">
 					@include('partials.svg.star')
 				</div>
 				<h3 class="sm:max-w-[145px] mx-auto mt-4 font-semibold text-center lg:max-w-xs">{{ __('Brand-new developers') }}</h3>
 			</div>
 		</div>
 
-		<div class="p-1 rounded-md bg-gradient-to-r from-teal via-teal to-violet">
+		<div class="p-1 rounded-md bg-gradient-to-r from-mint via-mint to-violet">
 			<div class="py-16 text-2xl rounded-md bg-blue-black lg:h4">
 				<div
-					class="flex items-center justify-center w-16 h-16 mx-auto rounded-full bg-teal bg-teal-dark md:w-20 md:h-20">
+					class="flex items-center justify-center w-16 h-16 mx-auto rounded-full bg-mint md:w-20 md:h-20">
 					@include('partials.svg.code')
 				</div>
 				<h3 class="sm:max-w-[145px] mx-auto mt-4 font-semibold text-center lg:max-w-xs">{{ __('Frontend developers') }}</h3>
 			</div>
 		</div>
 
-		<div class="p-1 rounded-md bg-gradient-to-r from-teal via-teal to-violet">
+		<div class="p-1 rounded-md bg-gradient-to-r from-mint via-mint to-violet">
 			<div class="py-16 text-2xl rounded-md lg:h4 bg-blue-black">
 				<div
-					class="flex items-center justify-center w-16 h-16 mx-auto rounded-full bg-teal bg-teal-dark md:w-20 md:h-20">
+					class="flex items-center justify-center w-16 h-16 mx-auto rounded-full bg-mint md:w-20 md:h-20">
 					@include('partials.svg.wp')
 				</div>
 				<h3 class="sm:max-w-[145px] mx-auto mt-4 font-semibold text-center lg:max-w-xs">{{ __('WordPress developers') }}</h3>

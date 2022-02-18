4f826ead5cf5d0c044f7adc710a4b1484cef378e
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-02-18 12:45:59 -0600

    Add links to headers, add smooth scroll, remove superfluous file, begin new hero styles

--- resources/views/partials/welcome-page/welcome-hero.blade.php
+++ resources/views/partials/welcome-page/welcome-hero.blade.php
@@ -5,8 +5,7 @@
                 {{ __('Providing an easy entrance into Laravel for new developers.') }}
             </h1>
 
-            <a class="inline-block w-auto px-4 py-2 mt-8 mb-12 text-lg font-semibold text-center transition duration-150 ease-in-out border-2 text-blue-black under rounded-3xl border-mint bg-mint hover:no-underline hover:bg-transparent hover:text-mint"
-                href="#overview">
+            <a class="relative inline-block w-auto px-4 py-2 mt-8 mb-12 text-lg font-semibold text-center transition duration-150 ease-in-out border-2 text-blue-black under rounded-3xl border-mint bg-mint hover:no-underline hover:bg-transparent hover:text-mint" href="#overview" style="scroll-margin-top: 50px">
                 <span>{{ __('Learn more') }}</span>
             </a>
         </div>

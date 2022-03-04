00188eee9a5ceb43460a4eb4696f052b455cb6b0
Tammy Robinson <TammyTee@users.noreply.github.com>
2022-03-04 14:42:51 -0500

    Update database/seeders/modules/beginning-php.php

--- database/seeders/modules/beginning-php.php
+++ database/seeders/modules/beginning-php.php
@@ -113,11 +113,6 @@ return [
                 'url' => 'https://laravelpodcast.com/episodes/intro-to-composer-with-jordi-boggiano',
                 'type' => Resource::AUDIO_TYPE,
             ],
-            [
-                'name' => 'The Laravel Podcast - Packages, with Freek Van der Herten & Marcel Pociot',
-                'url' => 'https://laravelpodcast.com/episodes/packages-with-freek-van-der-herten-marcel-pociot',
-                'type' => Resource::AUDIO_TYPE,
-            ],
         ],
     ],
 ];

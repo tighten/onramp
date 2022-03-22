6abd2ed5f43e95e172929ee01254eaa21b7bbe33
Tammy Robinson <tammyprodev@gmail.com>
2022-03-22 11:47:02 -0400

    Add seeders for new modules

---
+++ database/seeders/modules/laravel-core.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Resource;
+
+return [
+    'name' => ['en' => 'Laravel Core', 'es' => ''],
+    'description' => [
+        'en' => '',
+        'es' => '',
+    ],
+    'skills' => [
+        ['es' => '', 'en' => ''],
+    ],
+    'resources' => [
+        'es' => [],
+        'en' => [
+            [
+                'name' => 'The Laravel Podcast - The Service Container, with Christoph Rumpel',
+                'url' => 'https://laravelpodcast.com/episodes/the-service-container-with-christoph-rumpel',
+                'type' => Resource::AUDIO_TYPE,
+            ],
+        ]
+    ],
+];
---
+++ database/seeders/modules/laravel-queues.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Resource;
+
+return [
+    'name' => ['en' => 'Laravel Queues', 'es' => ''],
+    'description' => [
+        'en' => '',
+        'es' => '',
+    ],
+    'skills' => [
+        ['es' => '', 'en' => ''],
+    ],
+    'resources' => [
+        'es' => [],
+        'en' => [
+            [
+                'name' => 'The Laravel Podcast - Queues, with Mohamed Said',
+                'url' => 'https://laravelpodcast.com/episodes/queues-with-mohamed-said',
+                'type' => Resource::AUDIO_TYPE,
+            ],
+        ]
+    ],
+];

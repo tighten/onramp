6d93b6373e0fbd83623075943818d2fbe78db9ad
Tammy Robinson <tammyprodev@gmail.com>
2022-03-22 11:47:02 -0400

    Add seeders for new modules

---
+++ database/seeders/modules/auth.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Resource;
+
+return [
+    'name' => ['en' => 'Auth', 'es' => ''],
+    'description' => [
+        'en' => '',
+        'es' => '',
+    ],
+    'skills' => [
+        ['es' => '', 'en' => ''],
+    ],
+    'resources' => [
+        'es' => [],
+        'en' => [
+            [
+                'name' => 'The Laravel Podcast - Presets & Jetstream, with Michael Dyrynda',
+                'url' => 'https://laravelpodcast.com/episodes/presets-jetstream-with-michael-dyrynda',
+                'type' => Resource::AUDIO_TYPE,
+            ],
+        ]
+    ],
+];
---
+++ database/seeders/modules/laravel-package-development.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Resource;
+
+return [
+    'name' => ['en' => 'Laravel Package Development', 'es' => ''],
+    'description' => [
+        'en' => '',
+        'es' => '',
+    ],
+    'skills' => [
+        ['es' => '', 'en' => ''],
+    ],
+    'resources' => [
+        'es' => [],
+        'en' => [
+            [
+                'name' => 'The Laravel Podcast - Packages, with Freek Van der Herten & Marcel Pociot',
+                'url' => 'https://laravelpodcast.com/episodes/packages-with-freek-van-der-herten-marcel-pociot',
+                'type' => Resource::AUDIO_TYPE,
+            ],
+        ]
+    ],
+];

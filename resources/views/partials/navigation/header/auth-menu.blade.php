c0a1c38119084c2a1b2c3f451bea9c167b57a8af
Marcy Acevedo <marcy.acevedo@tighten.co>
2022-04-22 12:30:01 -0500

    Format file, bring back logout buttons, remove unused partial. Start to alpine-ify menu

--- resources/views/partials/navigation/header/auth-menu.blade.php
+++ resources/views/partials/navigation/header/auth-menu.blade.php
@@ -1,4 +1,4 @@
-<menu-dropdown>
+{{-- <menu-dropdown>
 	<template v-slot:toggle="props">
 		<button
 			class="flex items-center justify-center w-12 h-12 hover:no-underline focus:outline-none"
@@ -21,4 +21,28 @@
 		href="{{ route_wlocale('logout') }}"
 		:logout="true">
 	</menu-dropdown-item>
-</menu-dropdown>
+</menu-dropdown> --}}
+
+
+<div x-data="{ open: false }" class="text-white">
+    <button class="flex items-center justify-center w-12 h-12 text-white hover:no-underline focus:outline-none" x-on:click="open = ! open">
+        <img class="rounded-full"
+            src="{{ auth()->user()->profile_picture }}"
+            alt="{{ auth()->user()->name }}">
+    </button>
+
+    <ul x-show="open">
+        <li text="{{ __('Profile') }}"
+            href="{{ route_wlocale('user.profile.show') }}">
+        </li>
+
+        <li text="{{ __('Preferences') }}"
+            href="{{ route_wlocale('user.preferences.index') }}">
+        </li>
+
+        <li text="{{ __('Log out') }}"
+            href="{{ route_wlocale('logout') }}"
+            :logout="true">
+        </li>
+    </ul>
+</div>

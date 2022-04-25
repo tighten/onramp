4801ec07cfb2c8aa070581b120d6037fd27701b8
Tammy Robinson <tammyprodev@gmail.com>
2022-04-25 13:32:13 -0400

    Create base button component

--- resources/views/components/button/primary.blade.php
+++ resources/views/components/button/primary.blade.php
@@ -1,12 +1,12 @@
 @props([
-    'type' => 'submit',
+    'href' => null,
 ])
 
-<span class="inline-flex rounded-md shadow-sm">
-    <button
-        type="{{ $type }}"
-        class="px-4 py-2 text-sm font-medium text-white no-underline transition duration-200 ease-in-out border border-transparent rounded-md  focus:bg-white focus:text-purple bg-purple hover:bg-white hover:text-purple hover:no-underline focus:outline-none border-purple active:bg-white active:text-purple"
-    >
-        {{ $slot }}
-    </button>
-</span>
+<x-button.base
+    :href="$href"
+    {{ $attributes->merge([
+        'class' => 'focus:bg-white focus:text-purple bg-purple hover:bg-white hover:text-purple hover:no-underline focus:outline-none border-purple active:bg-white active:text-purple'
+    ]) }}
+>
+    {{ $slot }}
+</x-button.base>

42fd3ce9ed13f2d6f74619a69d6e86853d1f9b51
Shawn Jones <shawn.greg.jones@gmail.com>
2022-05-19 14:31:00 -0500

    Reference facade by string key binding.

---
+++ app/Providers/LocalizationServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Providers;
+
+use App\Localization\Locale;
+use Illuminate\Support\ServiceProvider;
+
+class LocalizationServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->singleton('localization', function ($app) {
+            return new Locale();
+        });
+    }
+}

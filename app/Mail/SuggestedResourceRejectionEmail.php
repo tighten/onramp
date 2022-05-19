9bfcab7e75867d4c9ecb6f6031a50288a0d0681c
Shawn Jones <shawn.greg.jones@gmail.com>
2022-05-19 14:32:32 -0500

    tlint.

--- app/Mail/SuggestedResourceRejectionEmail.php
+++ app/Mail/SuggestedResourceRejectionEmail.php
@@ -10,7 +10,8 @@ use Illuminate\Queue\SerializesModels;
 
 class SuggestedResourceRejectionEmail extends Mailable
 {
-    use Queueable, SerializesModels;
+    use Queueable;
+    use SerializesModels;
 
     protected $suggestedResource;
     protected $user;

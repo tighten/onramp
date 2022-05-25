7405e5e99c287fc210594628622b2948a399db10
Shawn Jones <shawn.greg.jones@gmail.com>
2022-05-25 11:24:47 -0500

    Update suggested resources shortcuts card for v4.

--- nova-components/SuggestedResourcesShortcuts/resources/js/components/Card.vue
+++ nova-components/SuggestedResourcesShortcuts/resources/js/components/Card.vue
@@ -1,29 +1,29 @@
 <template>
-    <card class="flex flex-col">
-        <div class="px-3 py-3">
+  <Card class="flex flex-col items-center justify-center">
+    <div class="px-3 py-3">
             <h1 class="mb-2 text-2xl font-light text-center text-90">Resource Suggestions</h1>
-            <p class="mb-2 leading-normal">Hey friends! If you'd like to contribute to Onramp, you can suggest resources for us to include here:</p>
-            <p class="leading-normal">
-                <a href="/nova/resources/suggested-resources/new">Suggest a new resource</a> |
-                <a href="/nova/resources/suggested-resources/">See all suggested resources</a>
+            <p class="mb-2 leading-normal text-center">Hey friends! If you'd like to contribute to Onramp, you can suggest resources for us to include here:</p>
+            <p class="leading-normal text-center">
+                <a class="font-bold text-primary-500" href="/nova/resources/suggested-resources/new">Suggest a new resource</a> |
+                <a class="font-bold text-primary-500" href="/nova/resources/suggested-resources/">See all suggested resources</a>
             </p>
         </div>
-    </card>
+  </Card>
 </template>
 
 <script>
 export default {
-    props: [
-        'card',
+  props: [
+    'card',
 
-        // The following props are only available on resource detail cards...
-        // 'resource',
-        // 'resourceId',
-        // 'resourceName',
-    ],
+    // The following props are only available on resource detail cards...
+    // 'resource',
+    // 'resourceId',
+    // 'resourceName',
+  ],
 
-    mounted() {
-        //
-    },
+  mounted() {
+    //
+  },
 }
 </script>
--- nova-components/SuggestedResourcesShortcuts/routes/api.php
+++ nova-components/SuggestedResourcesShortcuts/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Card API Routes

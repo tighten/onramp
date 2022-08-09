2bf7a9bd334b690285cca5f26b369bbf4246e2db
Tammy Robinson <tammyprodev@gmail.com>
2022-08-09 10:26:41 -0400

    Track resources expiring within two weeks

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use App\Completable;
 use App\Facades\Preferences;
 use App\OperatingSystem;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,13 +12,13 @@ class Resource extends Model implements Completable
 {
     use HasFactory;
 
-    const VIDEO_TYPE = 'video';
-    const COURSE_TYPE = 'course';
-    const AUDIO_TYPE = 'audio';
-    const BOOK_TYPE = 'book';
-    const ARTICLE_TYPE = 'article';
+    public const VIDEO_TYPE = 'video';
+    public const COURSE_TYPE = 'course';
+    public const AUDIO_TYPE = 'audio';
+    public const BOOK_TYPE = 'book';
+    public const ARTICLE_TYPE = 'article';
 
-    const TYPES = [
+    public const TYPES = [
         self::VIDEO_TYPE,
         self::COURSE_TYPE,
         self::AUDIO_TYPE,
@@ -78,7 +77,15 @@ class Resource extends Model implements Completable
 
     public function scopeExpired($query)
     {
-        return $query->where('expiration_date', '<', Carbon::now()->toDateTimeString());
+        return $query->where('expiration_date', '<', now()->toDateTimeString());
+    }
+
+    public function scopeExpiring($query)
+    {
+        return $query->whereBetween('expiration_date', [
+            now()->toDateTimeString(),
+            now()->addDays(15)->toDateTimeString(),
+        ]);
     }
 
     public function getIsNewAttribute()
@@ -86,16 +93,9 @@ class Resource extends Model implements Completable
         return $this->created_at->diffInDays(now()) <= 14;
     }
 
-    public function getDaysExpiredAttribute()
+    public function getDaysTilExpiredAttribute()
     {
-        if (! $this->isExpired()) {
-            return '0 Days';
-        }
-
-        $daysExpired = $this->expiration_date->diffInDays(now());
-        $daysLabel = $daysExpired === 1 ? ' Day' : ' Days';
-
-        return $daysExpired . $daysLabel;
+        return $this->expiration_date->diffForHumans();
     }
 
     public function isAssignedToAModule()
@@ -106,5 +106,13 @@ class Resource extends Model implements Completable
     public function isExpired()
     {
         return $this->expiration_date->isPast();
+    }
+
+    public function isExpiring()
+    {
+        return $this->expiration_date->between(
+            now()->toDateTimeString(),
+            now()->addDays(15)->toDateTimeString(),
+        );
     }
 }

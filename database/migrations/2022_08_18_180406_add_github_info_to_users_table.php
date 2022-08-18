8dbc432b4ed070cb43b319018385d86ac01217ab
Tammy Robinson <tammyprodev@gmail.com>
2022-08-18 14:10:23 -0400

    Add migration to add columns for github info to users the table

---
+++ database/migrations/2022_08_18_180406_add_github_info_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('github_username')->nullable();
+            $table->string('github_avatar')->nullable();
+            $table->string('github_user_id')->nullable();
+            $table->string('github_token', 1000)->nullable();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('github_username');
+            $table->dropColumn('github_avatar');
+            $table->dropColumn('github_user_id');
+            $table->dropColumn('github_token');
+        });
+    }
+};

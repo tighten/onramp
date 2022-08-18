5779b55cd19b565fe0444bddc696a61726d52ef4
Tammy Robinson <tammyprodev@gmail.com>
2022-08-18 14:11:08 -0400

    Update login form with Github sign in button

--- config/services.php
+++ config/services.php
@@ -47,4 +47,10 @@ return [
     'slack' => [
         'notification_endpoint' => env('SLACK_ENDPOINT'),
     ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URL'),
+    ],
 ];
--- resources/views/partials/navigation/header/guest-menu.blade.php
+++ resources/views/partials/navigation/header/guest-menu.blade.php
@@ -1,6 +1,6 @@
 <div class="flex w-full">
     <a class="flex-1 inline-block w-auto px-4 py-1 mr-4 text-lg font-semibold text-center transition duration-200 ease-in-out bg-transparent border-2 hover:no-underline rounded-3xl text-mint border-mint hover:bg-mint hover:text-blue-black whitespace-nowrap"
         href="{{ route_wlocale('login') }}">
-        <span>{{ __('Log in') }} / {{ __('Register') }}</span>
+        <span>{{ __('Get Started') }}</span>
     </a>
 </div>

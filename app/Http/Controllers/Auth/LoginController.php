54fcd237e50dbb3ef19501c5ff0410150e7275f0
Tammy Robinson <tammyprodev@gmail.com>
2022-08-22 14:18:28 -0500

    [wip] Store github user

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
 use App\Facades\Preferences;
 use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -18,7 +20,6 @@ class LoginController extends Controller
     | to conveniently provide its functionality to your applications.
     |
     */
-
     use AuthenticatesUsers;
 
     public function redirectTo()
@@ -46,6 +47,38 @@ class LoginController extends Controller
         return view('auth.login', [
             'pageTitle' => __('Log in'),
         ]);
+    }
+
+    public function redirectToProvider()
+    {
+        session()->flash('url.intended', route_wlocale('wizard.index'));
+
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('github')->user();
+
+        if (! User::where('github_user_id', $user->getId())->exists()) {
+            if (! User::where('github_username', $user->getNickname())->exists()) {
+                User::create([
+                    'name' => $user->getName(),
+                    'email' => $user->getEmail(),
+                    'github_username' => $user->getNickname(),
+                    'github_avatar' => $user->getAvatar(),
+                    'github_token' => encrypt($user->token),
+                    'github_user_id' => $user->getId(),
+                ]);
+            } else {
+                $storedUser = User::where('github_username', $user->getNickname());
+                $storedUser->update(['github_user_id' => $user->getId()]);
+            }
+        }
+
+        Auth::login(User::firstWhere('github_user_id', $user->getId()), true);
+
+        return redirect()->intended();
     }
 
     /**
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     ];
 
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'github_token',
     ];
 
     protected $casts = [

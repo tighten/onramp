852577cfedf02c2e08a79cc6c1d68728e54c2c81
Tammy Robinson <tammyprodev@gmail.com>
2022-09-30 11:45:19 -0500

    Add misc style adjustments

--- resources/views/partials/navigation/footer.blade.php
+++ resources/views/partials/navigation/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="w-full px-4 py-10 text-white bg-blue-black">
     <div class="flex flex-col items-center">
-        <p class="w-full mb-4 sm:w-auto sm:mb-0 text-body">
+        <p class="w-full mb-4 text-lg text-center sm:w-auto sm:mb-0 md:text-body">
             {{ __('From the lovely folks at') }}
 
             <a class="font-semibold" href="https://tighten.com/">
-                <span class="underline">Tighten.</span>
+                <span class="underline">Tighten</span>
             </a>
         </p>
 

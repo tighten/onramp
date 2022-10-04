d98d78cd1384259d7e2b3dd61b59678e4b6b2c02
Tammy Robinson <tammyprodev@gmail.com>
2022-10-04 11:00:12 -0500

    Only show back to top on mods index and glossary

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,4 +1,4 @@
-<section id="top" class="grid w-full grid-cols-1 pt-12 pb-8 overflow-x-hidden text-center text-white md:px-0 md:pb-0 sm:overflow-visible md:grid-cols-4 bg-blue-black">
+<section class="grid w-full grid-cols-1 pt-12 pb-8 overflow-x-hidden text-center text-white md:px-0 md:pb-0 sm:overflow-visible md:grid-cols-4 bg-blue-black" {{ $attributes }}>
     <div class="mx-auto md:ml-0 md:place-self-start">
         @include('partials.svg.path-sm')
     </div>

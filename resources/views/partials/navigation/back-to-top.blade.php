8f21d294f04573214175d453ead6dbe3c0115549
Tammy Robinson <tammyprodev@gmail.com>
2022-10-04 10:52:48 -0500

    Update back to top button styles

--- resources/views/partials/navigation/back-to-top.blade.php
+++ resources/views/partials/navigation/back-to-top.blade.php
@@ -1,10 +1,9 @@
 <div class="sticky bottom-12">
     <a
-        class="absolute invisible inline-block px-4 py-2 text-sm font-medium text-white no-underline uppercase transition duration-200 ease-in-out border border-transparent rounded-md right-3 bottom-3 bg-purple focus:bg-white focus:text-purple hover:bg-white hover:text-purple hover:no-underline focus:outline-none border-purple active:bg-white active:text-purple"
+        class="absolute inline-flex items-center justify-center invisible w-12 h-12 px-4 py-2 text-sm font-medium text-white no-underline uppercase transition duration-200 ease-in-out border border-transparent rounded-full right-3 bottom-3 bg-purple focus:bg-white focus:text-purple hover:bg-white hover:text-purple hover:no-underline focus:outline-none border-purple active:bg-white active:text-purple"
         href="#top"
         id="top-button"
     >
-        <span class="block font-semibold text-center transform -rotate-90">&gt;</span>
-        <span class="block font-semibold text-center">{{ __('Top') }}</span>
+        <span class="text-lg fa fa-chevron-up"></span>
     </a>
 </div>

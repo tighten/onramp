25f4c789c15b8b3d57bce52ed50b3c308392ad2e
Tammy Robinson <tammyprodev@gmail.com>
2022-10-31 12:29:32 -0400

    Add config value for default resource expiration length

--- app/Nova/Actions/RenewResource.php
+++ app/Nova/Actions/RenewResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Nova\Actions;
 
+use App\Models\Resource;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Collection;
 use Laravel\Nova\Actions\Action;
@@ -18,7 +18,7 @@ class RenewResource extends Action
     public function handle(ActionFields $fields, Collection $models)
     {
         foreach ($models as $model) {
-            $model->expiration_date = now()->addMonths(6);
+            $model->expiration_date = Resource::getNewExpirationDate();
 
             $model->save();
         }

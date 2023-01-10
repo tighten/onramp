9e1d131b799a9b93429f5fb51c33b645b3f18472
Tammy Robinson <tammyprodev@gmail.com>
2023-01-10 13:41:35 -0500

    Add seeder config file and update fresh install seeder

--- database/seeders/FreshInstallSeeder.php
+++ database/seeders/FreshInstallSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
-use App\Models\Track;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class FreshInstallSeeder extends Seeder
 {
@@ -16,16 +20,59 @@ class FreshInstallSeeder extends Seeder
             'role' => 'admin',
         ]);
 
-        $tracks = [
-            'New to Programming',
-            'Frontend Developers',
-            'WordPress Developers',
-        ];
+        $seedsDirectory = config('seeder.directory', 'database/json');
 
-        foreach ($tracks as $track) {
-            Track::factory()->create(['name' => $track]);
+        $seeds = $this->getSeedFiles($seedsDirectory);
+
+        if (! $seeds) {
+            $this->command->warn('The directory ' . $seedsDirectory . ' is empty.');
+            $this->command->line('You can create seeds from the production database by calling <info>php artisan generate:seeds-from-db --all</info>');
+
+            return 0;
         }
 
-        $this->call(ExistingContentSeeder::class);
+        $this->seed($seeds);
+    }
+
+    protected function seed(array $seeds)
+    {
+        Schema::disableForeignKeyConstraints();
+
+        foreach ($seeds as $seed) {
+            $filename = $seed->getFilename();
+            $tableName = Str::before($filename, '.' . config('seeder.extension'));
+
+            $this->command->line("Seeding $tableName...");
+
+            $path = $seed->getRealPath();
+            $content = File::get($path);
+            $rows = json_decode($content, 1);
+
+            DB::table($tableName)->truncate();
+
+            if (empty($rows)) {
+                continue;
+            }
+
+            foreach ($rows as $row) {
+                try {
+                    DB::table($tableName)->insert($row);
+                } catch (\Exception $e) {
+                    Log::warning($e->getMessage());
+                    break;
+                }
+            }
+
+            $this->command->info('Seeding successful!' . PHP_EOL);
+        }
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    protected function getSeedFiles(string $dir): array
+    {
+        $files = File::files($dir);
+
+        return array_values($files);
     }
 }

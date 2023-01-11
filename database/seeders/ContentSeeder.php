5c02f5a527de8f0066b14a38f3a879b39353040d
Tammy Robinson <tammyprodev@gmail.com>
2023-01-11 08:22:07 -0500

    Move seed operations to a trait. Update command to seed db when syncing individual tables.

---
+++ database/seeders/ContentSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Concerns\CanSeedProdData;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class ContentSeeder extends Seeder
+{
+    use CanSeedProdData;
+
+    public function run()
+    {
+        User::factory()->create([
+            'email' => 'matt@tighten.co',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+        $seedsDirectory = config('seeder.directory', 'database/json');
+
+        $seeds = $this->getSeedFiles($seedsDirectory);
+
+        if (! $seeds) {
+            $this->command->warn('The directory ' . $seedsDirectory . ' is empty.');
+            $this->command->line('You can create seeds from the production database by calling <info>php artisan generate:seeds-from-db --all</info>');
+
+            return 0;
+        }
+
+        $this->seed($this->command, $seeds);
+    }
+}

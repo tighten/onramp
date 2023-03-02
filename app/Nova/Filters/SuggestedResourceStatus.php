c1dbf9b18a9532e97b9372dd920ba368baee4a8d
Tammy Robinson <8689444+tammytee@users.noreply.github.com>
2023-03-02 12:21:29 -0500

    Fix filters

--- app/Nova/Filters/SuggestedResourceStatus.php
+++ app/Nova/Filters/SuggestedResourceStatus.php
@@ -4,8 +4,8 @@ namespace App\Nova\Filters;
 
 use App\Models\SuggestedResource;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Http\Request;
 use Laravel\Nova\Filters\Filter;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class SuggestedResourceStatus extends Filter
 {
@@ -19,7 +19,7 @@ class SuggestedResourceStatus extends Filter
      *
      * @param  mixed  $value
      */
-    public function apply(Request $request, Builder $query, $value): Builder
+    public function apply(NovaRequest $request, $query, $value): Builder
     {
         return $query->where('status', $value);
     }
@@ -27,7 +27,7 @@ class SuggestedResourceStatus extends Filter
     /**
      * Get the filter's available options.
      */
-    public function options(Request $request): array
+    public function options(NovaRequest $request): array
     {
         return [
             'Approved' => SuggestedResource::APPROVED_STATUS,

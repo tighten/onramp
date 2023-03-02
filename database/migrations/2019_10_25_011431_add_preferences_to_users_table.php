816a89f9a2e4dd54366216e91c026a3788a968c3
Shift <shift@laravelshift.com>
2023-03-02 14:32:41 +0000

    Adopt anonymous migrations

--- database/migrations/2019_10_25_011431_add_preferences_to_users_table.php
+++ database/migrations/2019_10_25_011431_add_preferences_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddPreferencesToUsersTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class AddPreferencesToUsersTable extends Migration
             $table->dropColumn('preferences');
         });
     }
-}
+};
--- database/migrations/2020_03_20_183815_create_module_resource_table.php
+++ database/migrations/2020_03_20_183815_create_module_resource_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateModuleResourceTable extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -20,4 +20,4 @@ class CreateModuleResourceTable extends Migration
     {
         Schema::dropIfExists('module_resource');
     }
-}
+};

f6eca960eb938acb12069a917b507bd295ef5ee0
Matt Stauffer <matt@tighten.co>
2024-06-04 11:28:13 -0400

    Fix wizard store method, and add test coverage

--- tests/Feature/WizardTest.php
+++ tests/Feature/WizardTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Facades\Preferences;
+use App\Models\Track;
 use App\Models\User;
+use App\OperatingSystem;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -18,5 +21,24 @@ class WizardTest extends TestCase
         $response = $this->get(route('wizard.index', ['locale' => 'en']));
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function it_can_be_submitted(): void
+    {
+        $this->be($user = User::factory()->create());
+        $response = $this->post(route('wizard.store', ['locale' => 'en']),
+            [
+            'os' => OperatingSystem::MACOS,
+            'track' => $track_id = Track::factory()->create()->id,
+            'locale' => 'en',
+            ]
+        );
+
+        $response->assertStatus(302);
+        $user->refresh();
+        $this->assertEquals($track_id, $user->track_id);
+        $this->assertEquals(OperatingSystem::MACOS, Preferences::get('operating-system'));
+        $this->assertEquals('en', Preferences::get('locale'));
     }
 }

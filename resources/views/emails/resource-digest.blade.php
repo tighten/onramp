bb1ef5eda399ca0d0d96ec7938d28c41d73a6372
Marcy Acevedo <marcy.acevedo@tighten.co>
2024-06-25 12:19:20 -0500

    Start mailale, template. Update DB

---
+++ resources/views/emails/resource-digest.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Your Title </title>
+    </head>
+
+    <body>
+        <h1>{{ $details['title'] }} </ h1>
+            <p>{{ $details['body'] }} </ p>
+    </body>
+
+</html>

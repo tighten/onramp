d834360d8bdaef58e4776fbb863a9517391110d4
Marcy Acevedo <marcy.acevedo@tighten.co>
2024-08-09 12:47:10 -0500

    Wire up the input

--- resources/views/components/button/primary.blade.php
+++ resources/views/components/button/primary.blade.php
@@ -1,12 +1,10 @@
 @props([
-    'href' => null,
+'href' => null,
 ])
 
-<x-button.base
-    :href="$href"
-    {{ $attributes->merge([
-        'class' => 'focus:bg-white focus:text-purple bg-purple hover:bg-white hover:text-purple hover:no-underline focus:outline-none border-purple active:bg-white active:text-purple'
-    ]) }}
->
-    {{ $slot }}
+<x-button.base :href="$href" {{ $attributes->merge([
+	'class' => 'focus:bg-white focus:text-purple bg-purple hover:bg-white hover:text-purple hover:no-underline focus:outline-none hover:border-purple focus:border-purple active:bg-white active:text-purple'
+	]) }}
+	>
+	{{ $slot }}
 </x-button.base>

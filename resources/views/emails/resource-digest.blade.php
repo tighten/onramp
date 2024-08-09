48d13f7be7298b6635456934e9d32837c3bfbc3f
Marcy Acevedo <marcy.acevedo@tighten.co>
2024-08-09 13:34:24 -0500

    Format email template

--- resources/views/emails/resource-digest.blade.php
+++ resources/views/emails/resource-digest.blade.php
@@ -3,7 +3,8 @@
 
 @component('mail::panel')
 @foreach ($resources as $resource)
-- {{ $resource['name'] }}
+- [{{ $resource['name'] }}]({{ $resource['url'] }})<br>
+Added on {{ \Carbon\Carbon::parse($resource['created_at'])->format('F j, Y') }}<br><br>
 @endforeach
 @endcomponent
 

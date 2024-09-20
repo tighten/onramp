a0b8947951bedb1d1c2116b2c22c5766dd351cc8
Marcy Acevedo <marcy.acevedo@tighten.co>
2024-09-20 11:19:28 -0500

    Add testing

---
+++ tests/Feature/ResourceDigestEmailTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Mail\ResourceDigestEmail;
+use App\Models\Resource;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ResourceDigestEmailTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function resource_digest_email_content()
+    {
+        $resources = Resource::factory()->count(3)->make();
+        $user = User::factory()->make(['locale' => 'en']);
+        $unsubscribeUrl = 'http://example.com/unsubscribe';
+
+        $mailable = new ResourceDigestEmail($resources, $user, $unsubscribeUrl);
+
+        $this->assertEquals('New Onramp Resources!', $mailable->envelope()->subject);
+
+        $renderedMailable = $mailable->render();
+
+        $this->assertStringContainsString($resources[0]->name, $renderedMailable);
+        $this->assertStringContainsString($unsubscribeUrl, $renderedMailable);
+    }
+}

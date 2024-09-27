f09ab5f509efac6ef9ef08dff31880f841f7b777
MarcyLina <MarcyLina@users.noreply.github.com>
2024-09-27 17:11:28 +0000

    Dusting

--- database/migrations/2024_06_25_171615_add_is_subscriber_to_users_table.php
+++ database/migrations/2024_06_25_171615_add_is_subscriber_to_users_table.php
@@ -6,17 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-	public function up(): void
-	{
-		Schema::table('users', function (Blueprint $table) {
-			$table->boolean('is_subscriber')->after('email_verified_at')->default(false);
-		});
-	}
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_subscriber')->after('email_verified_at')->default(false);
+        });
+    }
 
-	public function down(): void
-	{
-		Schema::table('users', function (Blueprint $table) {
-			$table->dropColumn('is_subscriber');
-		});
-	}
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_subscriber');
+        });
+    }
 };

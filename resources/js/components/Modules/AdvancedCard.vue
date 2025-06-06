1cc5deb4390f74265763d8108e67a0f6ac3365ef
Marcy Acevedo <marcy.acevedo@tighten.co>
2025-06-06 11:31:20 -0500

    Clean up. get modules working.

---
+++ resources/js/components/Modules/AdvancedCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <p v-if="!advancedModules.length" class="px-3 text-steel">
+        There are currently no modules here. Check back soon.
+    </p>
+    <ModuleCard
+        v-else
+        v-for="(mod, index) in advancedModules"
+        :key="mod.id"
+        :item="mod"
+        :card-is-even="index % 2 === 0"
+        level="advanced"
+        :completed-resources-count="getModuleCompletedResources(mod)"
+        :is-user-module="userModules.includes(mod.id)"
+        :is-completed="getModuleIsCompleted(mod)"
+        :has-new-content="moduleHasNewResources(mod)"
+    />
+</template>
+
+<script setup>
+import ModuleCard from './ModuleCard.vue';
+
+const props = defineProps({
+    advancedModules: Array,
+    userModules: Array,
+    userLoggedIn: Boolean,
+    getModuleCompletedResources: Function,
+    getModuleIsCompleted: Function,
+    moduleHasNewResources: Function,
+});
+</script>
---
+++ resources/js/components/Modules/IntermediateCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <p v-if="!intermediateModules.length" class="px-3 text-steel">
+        There are currently no modules here. Check back soon.
+    </p>
+    <ModuleCard
+        v-else
+        v-for="(mod, index) in intermediateModules"
+        :key="mod.id"
+        :item="mod"
+        :card-is-even="index % 2 === 0"
+        level="intermediate"
+        :completed-resources-count="getModuleCompletedResources(mod)"
+        :is-user-module="userModules.includes(mod.id)"
+        :is-completed="getModuleIsCompleted(mod)"
+        :has-new-content="moduleHasNewResources(mod)"
+    />
+</template>
+
+<script setup>
+import ModuleCard from './ModuleCard.vue';
+
+const props = defineProps({
+    intermediateModules: Array,
+    userModules: Array,
+    userLoggedIn: Boolean,
+    getModuleCompletedResources: Function,
+    getModuleIsCompleted: Function,
+    moduleHasNewResources: Function,
+});
+</script>

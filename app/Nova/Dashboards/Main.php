9a993b7fa9418710cae39ce12dd57ef1131cc854
Marcy Acevedo <marcy.acevedo@tighten.co>
2025-10-09 10:44:11 -0500

    update dashboard

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,20 +2,20 @@
 
 namespace App\Nova\Dashboards;
 
+use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Dashboards\Main as Dashboard;
-use Tightenco\NovaReleases\LatestRelease;
-use Tightenco\SuggestedResourcesShortcuts\SuggestedResourcesShortcuts;
 
 class Main extends Dashboard
 {
     /**
      * Get the cards for the dashboard.
+     *
+     * @return array
      */
-    public function cards(): array
+    public function cards()
     {
         return [
-            new SuggestedResourcesShortcuts,
-            // new LatestRelease(),
+            new Main,
         ];
     }
 }

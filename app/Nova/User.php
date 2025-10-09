7731b79194fe032cf856e2b138d1aa28c017aefc
Marcy Acevedo <marcy.acevedo@tighten.co>
2025-10-09 10:47:55 -0500

    Revert changes made by nova install

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -1,110 +1,83 @@
 <?php
 
-namespace App\Nova;
+    namespace App\Nova;
 
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rules;
-use Laravel\Nova\Fields\Gravatar;
-use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Password;
-use Laravel\Nova\Fields\Text;
+    use Illuminate\Http\Request;
+    use Laravel\Nova\Fields\Gravatar;
+    use Laravel\Nova\Fields\ID;
+    use Laravel\Nova\Fields\Password;
+    use Laravel\Nova\Fields\Select;
+    use Laravel\Nova\Fields\Text;
 
-class User extends Resource
-{
-    /**
-     * The model the resource corresponds to.
-     *
-     * @var class-string<\App\Models\User>
-     */
-    public static $model = \App\Models\User::class;
-
-    /**
-     * The single value that should be used to represent the resource when being displayed.
-     *
-     * @var string
-     */
-    public static $title = 'name';
-
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
-    public static $search = [
-        'id', 'name', 'email',
-    ];
-
-    /**
-     * Get the fields displayed by the resource.
-     *
-     * @param  \Illuminate\Http\Request   $request
-     * @return array
-     */
-    public function fields(Request $request): array
+    class User extends BaseResource
     {
-        return [
-            ID::make()->sortable(),
+        public static $model = \App\Models\User::class;
 
-            Gravatar::make()->maxWidth(50),
+        public static $title = 'name';
 
-            Text::make('Name')
-                ->sortable()
-                ->rules('required', 'max:255'),
-
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
-
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', Rules\Password::defaults())
-                ->updateRules('nullable', Rules\Password::defaults()),
+        public static $search = [
+            'id', 'name', 'email',
         ];
-    }
 
-    /**
-     * Get the cards available for the request.
-     *
-     * @param  \Illuminate\Http\Request   $request
-     * @return array
-     */
-    public function cards(Request $request): array
-    {
-        return [];
-    }
+        public function fields(Request $request)
+        {
+            return [
+                ID::make()->sortable(),
 
-    /**
-     * Get the filters available for the resource.
-     *
-     * @param  \Illuminate\Http\Request   $request
-     * @return array
-     */
-    public function filters(Request $request): array
-    {
-        return [];
-    }
+                Gravatar::make(),
 
-    /**
-     * Get the lenses available for the resource.
-     *
-     * @param  \Illuminate\Http\Request   $request
-     * @return array
-     */
-    public function lenses(Request $request): array
-    {
-        return [];
-    }
+                Text::make('Name')
+                    ->sortable()
+                    ->rules('required', 'max:255'),
 
-    /**
-     * Get the actions available for the resource.
-     *
-     * @param  \Illuminate\Http\Request   $request
-     * @return array
-     */
-    public function actions(Request $request): array
-    {
-        return [];
+                Text::make('Email')
+                    ->sortable()
+                    ->rules('required', 'email', 'max:254')
+                    ->creationRules('unique:users,email')
+                    ->updateRules('unique:users,email,{{resourceId}}'),
+
+                Password::make('Password')
+                    ->onlyOnForms()
+                    ->creationRules('required', 'string', 'min:8')
+                    ->updateRules('nullable', 'string', 'min:8'),
+
+                Select::make('Role')->options([
+                    'admin' => 'Admin',
+                    'editor' => 'Editor',
+                    'user' => 'User',
+                ]),
+            ];
+        }
+
+        /**
+         * Get the cards available for the request.
+         */
+        public function cards(Request $request): array
+        {
+            return [];
+        }
+
+        /**
+         * Get the filters available for the resource.
+         */
+        public function filters(Request $request): array
+        {
+            return [];
+        }
+
+        /**
+         * Get the lenses available for the resource.
+         */
+        public function lenses(Request $request): array
+        {
+            return [];
+        }
+
+        /**
+         * Get the actions available for the resource.
+         */
+        public function actions(Request $request): array
+        {
+            return [];
+        }
     }
-}

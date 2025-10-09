61eae9737868a48cb5a4dee1e43a0195ae0bbf72
MarcyLina <MarcyLina@users.noreply.github.com>
2025-10-09 16:07:13 +0000

    Dusting

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -1,83 +1,83 @@
 <?php
 
-    namespace App\Nova;
+namespace App\Nova;
 
-    use Illuminate\Http\Request;
-    use Laravel\Nova\Fields\Gravatar;
-    use Laravel\Nova\Fields\ID;
-    use Laravel\Nova\Fields\Password;
-    use Laravel\Nova\Fields\Select;
-    use Laravel\Nova\Fields\Text;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Password;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
 
-    class User extends BaseResource
+class User extends BaseResource
+{
+    public static $model = \App\Models\User::class;
+
+    public static $title = 'name';
+
+    public static $search = [
+        'id', 'name', 'email',
+    ];
+
+    public function fields(Request $request)
     {
-        public static $model = \App\Models\User::class;
+        return [
+            ID::make()->sortable(),
 
-        public static $title = 'name';
+            Gravatar::make(),
 
-        public static $search = [
-            'id', 'name', 'email',
+            Text::make('Name')
+                ->sortable()
+                ->rules('required', 'max:255'),
+
+            Text::make('Email')
+                ->sortable()
+                ->rules('required', 'email', 'max:254')
+                ->creationRules('unique:users,email')
+                ->updateRules('unique:users,email,{{resourceId}}'),
+
+            Password::make('Password')
+                ->onlyOnForms()
+                ->creationRules('required', 'string', 'min:8')
+                ->updateRules('nullable', 'string', 'min:8'),
+
+            Select::make('Role')->options([
+                'admin' => 'Admin',
+                'editor' => 'Editor',
+                'user' => 'User',
+            ]),
         ];
-
-        public function fields(Request $request)
-        {
-            return [
-                ID::make()->sortable(),
-
-                Gravatar::make(),
-
-                Text::make('Name')
-                    ->sortable()
-                    ->rules('required', 'max:255'),
-
-                Text::make('Email')
-                    ->sortable()
-                    ->rules('required', 'email', 'max:254')
-                    ->creationRules('unique:users,email')
-                    ->updateRules('unique:users,email,{{resourceId}}'),
-
-                Password::make('Password')
-                    ->onlyOnForms()
-                    ->creationRules('required', 'string', 'min:8')
-                    ->updateRules('nullable', 'string', 'min:8'),
-
-                Select::make('Role')->options([
-                    'admin' => 'Admin',
-                    'editor' => 'Editor',
-                    'user' => 'User',
-                ]),
-            ];
-        }
-
-        /**
-         * Get the cards available for the request.
-         */
-        public function cards(Request $request): array
-        {
-            return [];
-        }
-
-        /**
-         * Get the filters available for the resource.
-         */
-        public function filters(Request $request): array
-        {
-            return [];
-        }
-
-        /**
-         * Get the lenses available for the resource.
-         */
-        public function lenses(Request $request): array
-        {
-            return [];
-        }
-
-        /**
-         * Get the actions available for the resource.
-         */
-        public function actions(Request $request): array
-        {
-            return [];
-        }
     }
+
+    /**
+     * Get the cards available for the request.
+     */
+    public function cards(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     */
+    public function filters(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     */
+    public function lenses(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     */
+    public function actions(Request $request): array
+    {
+        return [];
+    }
+}

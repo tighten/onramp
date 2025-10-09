014fa6f064adbd640f844903abb309345dc182c9
Marcy Acevedo <marcy.acevedo@tighten.co>
2025-10-09 10:53:59 -0500

    Revert dash

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -3,18 +3,19 @@
 namespace App\Nova\Dashboards;
 
 use Laravel\Nova\Dashboards\Main as Dashboard;
+use Tightenco\NovaReleases\LatestRelease;
+use Tightenco\SuggestedResourcesShortcuts\SuggestedResourcesShortcuts;
 
 class Main extends Dashboard
 {
     /**
      * Get the cards for the dashboard.
-     *
-     * @return array
      */
-    public function cards()
+    public function cards(): array
     {
         return [
-            new Main,
+            new SuggestedResourcesShortcuts,
+            // new LatestRelease(),
         ];
     }
 }

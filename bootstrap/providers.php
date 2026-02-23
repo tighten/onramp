c10b48bb4488cf9be509ce329d9db673e20fe363
Shift <shift@laravelshift.com>
2026-02-23 23:57:19 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\NovaServiceProvider::class,
+    App\Providers\PreferencesServiceProvider::class,
+    App\Providers\LocalizationServiceProvider::class,
+    App\Providers\ViewServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -5,29 +5,6 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\NovaServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        App\Providers\PreferencesServiceProvider::class,
-        App\Providers\LocalizationServiceProvider::class,
-        App\Providers\ViewServiceProvider::class,
-        KgBot\LaravelLocalization\LaravelLocalizationServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'ExportLocalization' => 'KgBot\\LaravelLocalization\\Facades\\ExportLocalization',

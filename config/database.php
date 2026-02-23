8c932ea88aa3178e69017a6fbbceadfc371b3be7
Shift <shift@laravelshift.com>
2026-02-23 23:57:26 +0000

    Remove Doctrine DBAL type configuration

--- config/database.php
+++ config/database.php
@@ -26,10 +26,5 @@ return [
         'update_date_on_publish' => false, // disable to preserve original behavior for existing applications
     ],
 
-    'dbal' => [
-        'types' => [
-            'timestamp' => TimestampType::class,
-        ],
-    ],
 
 ];

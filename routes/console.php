00185a77283d06ed4168e04cf89e5d06138346fe
Shift <shift@laravelshift.com>
2026-02-23 23:57:20 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::command('resource:expired -N')
+    ->weeklyOn(Schedule::FRIDAY, '06:00');
+
+Schedule::command('mail:send-resource-digest-email')->monthly();
